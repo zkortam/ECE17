b8e4737deab0cabcedaccc623b7097d485734e6a
Zakaria Kortam <65579842+zkortam@users.noreply.github.com>
2025-01-02 23:40:44 -0800

    Create helpers.h

---
+++ ExceptionHandling/helpers.h
@@ -0,0 +1,11 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+#include "LinkedList.h"
+using namespace std;
+
+// declare helpers methods below
+LinkedList loadFile(const string& fileName);
+int searchList(const LinkedList& list, double price);
+
+
+#endif 
